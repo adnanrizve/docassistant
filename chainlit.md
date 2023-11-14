f24ffa54438b8b13dcd8c23ddbffc772a79efc5d
adnan rizve <75649781+adnanrizve@users.noreply.github.com>
2023-11-14 09:58:12 -0800

    Add files via upload

---
+++ chainlit.md
@@ -0,0 +1,7 @@
+These simple bot will allow you to chat with PDF files. The local LLM which these bots are going to use by default is https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF/blob/main/llama-2-7b-chat.Q8_0.gguf. I am using "BAAI/bge-small-en-v1.5" as embedding model due to its high score on leaderboard https://huggingface.co/spaces/mteb/leaderboard
+
+This repo have two apps (Chainlit based), you can run "allinone.py" through chainlit command if you would like to use GUI to upload the file and then chat with a document. The other app is "answerbot.py"  which allow you ask question regarding multiple documents but you need to use "ingest.py" to perform the embedding process.
+
+Special thanks to https://github.com/AIAnytime/Llama2-Medical-Chatbot and https://docs.chainlit.io/examples/qa
+
+Let's Go!
---
+++ ingest.py
@@ -0,0 +1,29 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter 
+
+DATA_PATH = 'localdata/'
+DB_FAISS_PATH = 'vectorstore/db_faiss'
+
+# Create vector database
+def create_vector_db():
+    loader = DirectoryLoader(DATA_PATH,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=400,
+                                                   chunk_overlap=80)
+    texts = text_splitter.split_documents(documents)
+
+    # you can use a different embedding by replacing with model_name = sentence-transformers/all-MiniLM-L6-v2. system will download it during first execution
+    embeddings = HuggingFaceEmbeddings(model_name='BAAI/bge-small-en-v1.5',
+                                       model_kwargs={'device': 'cpu'})
+
+    db = FAISS.from_documents(texts, embeddings)
+    db.save_local(DB_FAISS_PATH)
+
+if __name__ == "__main__":
+    create_vector_db()
+
